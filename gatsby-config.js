99f587c7232e6162452103284dc9f8e14d9750be
deekshasharma <deeksha.sharma25@gmail.com>
2020-12-14 15:47:46 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
