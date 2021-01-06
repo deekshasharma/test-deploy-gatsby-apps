8995f74a29857db6b14ddec4741280c79644f879
deekshasharma <deeksha.sharma25@gmail.com>
2021-01-06 13:27:58 -0800

    Added About Page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,54 @@
 import React from "react"
+import { PageLayout } from "../components/shared/PageLayout"
+import { Grid, makeStyles, Typography } from "@material-ui/core"
+import aboutCoffee from "../images/about-coffee.svg"
+
+const aboutText =
+  "Founded in the pristine soil of Vancouver Island, British Columbia, Mukti cafe aspires to offer the most delightful and sustainable coffee and to share our passion with those who care as deeply as we do.  With mindfulness and transparency as our values, we buy ethical beans from the farmers and follow fair trade practices.\n" +
+  "\n" +
+  "With an extensive collection of flavors, each cup of coffee is made with love by our very own creative and relentless baristas on site.  Our goal is to preserve the rich taste of our high-quality specialty coffee beans with the right amount of careful roasting to unlock every bean's full potential.\n" +
+  "\n"
+
+const conclusionText =
+  "We proudly curate our coffees into three different combinations (Black, Red, and Blue) based on their roasting profiles; we urge you to explore and discover this vibrant spectrum of taste experiences with valuable advice intended to help you get the most out of your daily routine. Flavour is a journey that matters only in the context of you and your satisfaction with the coffee cup in your hands. It is our privilege to help illuminate some light into the world of possibilities that coffee presents."
+
+const useStyles = makeStyles(theme => ({
+  aboutContainer: {
+    padding: "20px",
+  },
+  aboutImg: {
+    width: "50vw",
+    marginBottom: "10vh",
+    [theme.breakpoints.down("sm")]: {
+      width: "100vw",
+    },
+  },
+}))
 
 const About = () => {
-  return <div>Welcome to About Page</div>
+  const classes = useStyles()
+  return (
+    <PageLayout>
+      <Grid container justify="center" className={classes.aboutContainer}>
+        <Grid item>
+          <img
+            src={aboutCoffee}
+            alt="about-mukti-cafe"
+            className={classes.aboutImg}
+          />
+        </Grid>
+
+        <Grid item>
+          <Typography gutterBottom variant="subtitle1">
+            {aboutText}
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="subtitle1">{conclusionText}</Typography>
+        </Grid>
+      </Grid>
+    </PageLayout>
+  )
 }
 export default About
--- src/pages/eats.jsx
+++ src/pages/eats.jsx
@@ -1,7 +1,42 @@
 import React from "react"
-
+import { PageLayout } from "../components/shared/PageLayout"
+import { Grid, Typography } from "@material-ui/core"
+import { MenuCard } from "../components/shared/MenuCard"
+const allEats = [
+  { name: "Breakfast Sandwich", price: "$5.30" },
+  { name: "Sandwich", price: "$9.75" },
+  { name: "Pint of Gelato", price: "$12.70" },
+  { name: "Salad", price: "$16.00" },
+  { name: "Avocado Toast", price: "$8.00" },
+  { name: "Dessert Bars", price: "$4.00" },
+  { name: "Cookie", price: "$3.75" },
+  { name: "Biscotti", price: "$3.50" },
+  { name: "Chips", price: "$3.75" },
+]
 const Eats = () => {
-  return <div>Welcome to Eats Page</div>
+  return (
+    <PageLayout>
+      <div style={{ padding: "40px" }}>
+        <Grid container direction="column" justify="center" alignItems="center">
+          <Typography variant="h3">Eats</Typography>
+          <Typography variant="subtitle1" align="center">
+            Whilst we generally flaunt a much more extensive food menu, we are
+            just getting back into the swing of things and will progressively
+            add food items to our cafe as they become available!
+          </Typography>
+        </Grid>
+        <Grid container spacing={9} style={{ marginTop: "10vh" }}>
+          {allEats.map(drink => {
+            return (
+              <Grid item xs={12} md={4}>
+                <MenuCard name={drink.name} price={drink.price} />
+              </Grid>
+            )
+          })}
+        </Grid>
+      </div>
+    </PageLayout>
+  )
 }
 
 export default Eats
