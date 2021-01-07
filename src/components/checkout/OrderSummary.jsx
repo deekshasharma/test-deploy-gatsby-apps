9d3edb97d478bcb310977e044ba53f954ee998e9
deekshasharma <deeksha.sharma25@gmail.com>
2021-01-06 17:32:03 -0800

    Removed unused css

--- src/components/checkout/OrderSummary.jsx
+++ src/components/checkout/OrderSummary.jsx
@@ -1,26 +1,27 @@
 import React from "react"
 import { Divider, Grid, Typography } from "@material-ui/core"
-import styles from "./OrderSummary.module.css"
+import makeStyles from "@material-ui/core/styles/makeStyles"
 
-// const useStyles = makeStyles(theme => ({
-//   summaryContainer: {
-//     padding: "40px",
-//     border: "0.5px solid gray",
-//     maxWidth: "80vw",
-//   },
-//   summaryDivider: {
-//     padding: "5vh",
-//   },
-// }))
+const useStyles = makeStyles(theme => ({
+  summaryContainer: {
+    padding: "40px",
+    border: "0.5px solid gray",
+    maxWidth: "80vw",
+  },
+  summaryDivider: {
+    marginTop: "5vh",
+    marginBottom: "5vh",
+  },
+}))
 
 export const OrderSummary = ({ items }) => {
-  // const classes = useStyles()
+  const classes = useStyles()
   const getTotal = () => {
     return items.reduce((acc, item) => acc + parseFloat(item.price), 0)
   }
 
   return (
-    <Grid container direction="column" className={styles.summaryContainer}>
+    <Grid container direction="column" className={classes.summaryContainer}>
       <Grid item>
         <Typography gutterBottom variant="h6" align="center">
           ORDER SUMMARY
@@ -40,9 +41,7 @@ export const OrderSummary = ({ items }) => {
           )
         })}
       </Grid>
-      <Grid item className={styles.summaryDivider}>
-        <Divider />
-      </Grid>
+      <Divider className={classes.summaryDivider} />
       <Grid item>
         <Grid container justify="space-between">
           <Typography> ORDER TOTAL</Typography>
