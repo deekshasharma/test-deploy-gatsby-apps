db3e0cbd9002d03f27f482f3bbc2097005b9b1a1
deekshasharma <deeksha.sharma25@gmail.com>
2021-01-11 07:20:12 -0800

    Added tests for UserInfo component

---
+++ src/components/__tests__/UserInfo.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { UserInfo } from "../checkout/UserInfo"
+
+describe("UserInfo", () => {
+  const onClickOrder = jest.fn()
+  const OnChangeName = jest.fn()
+  const onChangePhone = jest.fn()
+  const disableOrder = true
+  const fullName = ""
+  const phone = ""
+
+  it("renders continue shopping link", () => {
+    render(
+      <UserInfo
+        onClickOrder={onClickOrder}
+        onChangeName={OnChangeName}
+        onChangePhone={onChangePhone}
+        disableOrder={disableOrder}
+        fullName={fullName}
+        phone={phone}
+      />
+    )
+    const linkText = screen.getByText("Continue Shopping")
+    expect(linkText).toBeInTheDocument()
+    expect(linkText.closest("a")).toHaveAttribute("href", "/menu")
+  })
+
+  it("renders full name and phone text fields", () => {
+    render(
+      <UserInfo
+        onClickOrder={onClickOrder}
+        onChangeName={OnChangeName}
+        onChangePhone={onChangePhone}
+        disableOrder={disableOrder}
+        fullName={fullName}
+        phone={phone}
+      />
+    )
+    const nameTextField = screen.getByLabelText("Full Name")
+    const phoneTextField = screen.getByLabelText("Phone Number")
+    expect(nameTextField).toBeInTheDocument()
+    expect(phoneTextField).toBeInTheDocument()
+  })
+
+  it("renders a disabled button", () => {
+    render(
+      <UserInfo
+        onClickOrder={onClickOrder}
+        onChangeName={OnChangeName}
+        onChangePhone={onChangePhone}
+        disableOrder={disableOrder}
+        fullName={fullName}
+        phone={phone}
+      />
+    )
+    const orderButton = screen.getByRole("button")
+    expect(orderButton).toBeDisabled()
+  })
+})
