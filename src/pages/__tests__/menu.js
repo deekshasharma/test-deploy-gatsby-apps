cdf6e76abd012ffe66fb1072069d4fecf3a88418
deekshasharma <deeksha.sharma25@gmail.com>
2021-01-10 18:23:04 -0800

    test data shared with multiple test suites and updated snapshot

--- src/pages/__tests__/menu.js
+++ src/pages/__tests__/menu.js
@@ -1,137 +1,82 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import renderer from "react-test-renderer"
-import { useStaticQuery } from "gatsby"
 import Menu from "../menu"
 
-// beforeEach(() => {
-//   useStaticQuery.mockImplementationOnce(({ render }) =>
-//     render({
-//       allMenuDataJson: {
-//         edges: [
-//           {
-//             node: {
-//               drinks: {
-//                 heading: "Drinks",
-//                 text: "Our Drink Menu",
-//                 allDrinks: [
-//                   {
-//                     name: "Drip Coffee",
-//                     price: "2.30",
-//                   },
-//                 ],
-//               },
-//             },
-//           },
-//           {
-//             node: {
-//               eats: {
-//                 heading: "Eats",
-//                 text: "Our Food Menu",
-//                 allEats: [
-//                   {
-//                     name: "Breakfast Sandwich",
-//                     price: "5.30",
-//                   },
-//                 ],
-//               },
-//             },
-//           },
-//         ],
-//       },
-//     })
-//   )
-// })
-
-beforeEach(() => {
-  useStaticQuery.mockReturnValue({
-    allMenuDataJson: {
-      edges: [
-        {
-          node: {
-            drinks: {
-              heading: "Drinks",
-              text: "Our Drink Menu",
-              allDrinks: [
-                {
-                  name: "Drip Coffee",
-                  price: "2.30",
-                },
-              ],
-            },
+const menuData = {
+  allMenuDataJson: {
+    edges: [
+      {
+        node: {
+          drinks: {
+            heading: "Drinks",
+            text: "Our Drink Menu",
+            allDrinks: [
+              {
+                name: "Drip Coffee",
+                price: "2.30",
+              },
+            ],
           },
         },
-        {
-          node: {
-            eats: {
-              heading: "Eats",
-              text: "Our Food Menu",
-              allEats: [
-                {
-                  name: "Breakfast Sandwich",
-                  price: "5.30",
-                },
-              ],
-            },
+      },
+      {
+        node: {
+          eats: {
+            heading: "Eats",
+            text: "Our Food Menu",
+            allEats: [
+              {
+                name: "Breakfast Sandwich",
+                price: "5.30",
+              },
+            ],
           },
         },
-      ],
-    },
-  })
-})
+      },
+    ],
+  },
+}
 
-describe("renders Menu snapshot correctly", () => {
-  it("renders Menu correctly", () => {
-    const tree = renderer.create(<Menu />).toJSON()
-    expect(tree).toMatchSnapshot()
+describe("header", () => {
+  // it("renders Mukti logo image", () => {
+  //   render(<Menu data={menuData} />)
+  //   const logo = screen.getByAltText("logo")
+  //   expect(logo).toBeInTheDocument()
+  // })
+
+  it("renders cart image", () => {
+    render(<Menu data={menuData} />)
+    const logo = screen.getByAltText("cart")
+    expect(logo).toBeInTheDocument()
   })
 })
 
 describe("renders menu cards", () => {
-  const menuData = {
-    allMenuDataJson: {
-      edges: [
-        {
-          node: {
-            drinks: {
-              heading: "Drinks",
-              text: "Our Drink Menu",
-              allDrinks: [
-                {
-                  name: "Drip Coffee",
-                  price: "2.30",
-                },
-              ],
-            },
-          },
-        },
-        {
-          node: {
-            eats: {
-              heading: "Eats",
-              text: "Our Food Menu",
-              allEats: [
-                {
-                  name: "Breakfast Sandwich",
-                  price: "5.30",
-                },
-              ],
-            },
-          },
-        },
-      ],
-    },
-  }
-  it("renders the drinks section correctly", () => {
-    render(<Menu data={menuData} />)
-    const drinksText = screen.getByText("Breakfast Sandwich")
-    const price = screen.getByText("5.30")
-    expect(drinksText).toBeInTheDocument()
-    expect(price).toBeInTheDocument()
+  it("renders Menu correctly", () => {
+    const tree = renderer.create(<Menu data={menuData} />).toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
-  // it("renders Menu correctly", () => {
-  //   const tree = renderer.create(<Menu data={menuData} />).toJSON()
-  //   expect(tree).toMatchSnapshot()
-  // })
+  it("renders the menu text correctly", () => {
+    render(<Menu data={menuData} />)
+    const eatMenuItem = screen.getByText("Breakfast Sandwich")
+    const eatItemPrice = screen.getByText("5.30")
+    const drinkHeading = screen.getByText("Drinks")
+    const drinkText = screen.getByText("Our Drink Menu")
+    expect(eatMenuItem).toBeInTheDocument()
+    expect(eatItemPrice).toBeInTheDocument()
+    expect(drinkHeading).toBeInTheDocument()
+    expect(drinkText).toBeInTheDocument()
+  })
+
+  it("renders correct number of buttons", () => {
+    render(<Menu data={menuData} />)
+    expect(screen.getAllByRole("button")).toHaveLength(6)
+  })
+
+  it("renders correct number add icons menu items", () => {
+    render(<Menu data={menuData} />)
+    expect(screen.getAllByAltText("add-item")).toHaveLength(2)
+  })
 })
