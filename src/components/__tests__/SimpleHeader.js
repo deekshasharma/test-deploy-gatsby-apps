8f152d1331c3e89bb985bf3880a16d66c0a850f1
deekshasharma <deeksha.sharma25@gmail.com>
2021-01-12 12:47:21 -0800

    updated snapshot and test

--- src/components/__tests__/SimpleHeader.js
+++ src/components/__tests__/SimpleHeader.js
@@ -2,7 +2,6 @@ import React from "react"
 import renderer from "react-test-renderer"
 import { SimpleHeader } from "../SimpleHeader"
 import { render, screen } from "@testing-library/react"
-import { MuktiLogo } from "../shared/MuktiLogo"
 
 describe("SimpleHeader Snapshot", () => {
   it("renders SimpleHeader correctly", () => {
@@ -11,7 +10,8 @@ describe("SimpleHeader Snapshot", () => {
   })
 
   test("render Mukti logo button", () => {
-    render(<MuktiLogo />)
+    render(<SimpleHeader />)
     expect(screen.getByRole("button")).toBeInTheDocument()
+    expect(screen.getByAltText("logo")).toBeInTheDocument()
   })
 })
